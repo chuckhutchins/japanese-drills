f7ac47441ad12e3968f9c9f8554a0b86eb85c868
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-01-20 12:49:54 -0500

    adding dayjs to project

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+// third-party plugins
+import dayjs from 'dayjs';
+
 const app = createApp(App);
+
+// dayjs config
+app.config.globalProperties.$dayjs = dayjs;
 
 app.use(createPinia());
 app.use(router);
