916ce9de3b4076d742c34b9e7526723fc85e0198
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-01-19 21:24:19 -0500

    adding basic settings store

--- src/stores/Settings.ts
+++ src/stores/Settings.ts
@@ -4,7 +4,16 @@ export const useSettingsStore = defineStore('settings', {
   state: () => ({
     currentDrill: '',
     style: 'hiragana',
+    settings: {
+      cycle: 'infinite',
+      order: 'random',
+      type: 'hiragana',
+    },
   }),
+  getters: {
+    isInfinite: (state) => state.settings.cycle === 'infinite',
+    isRandom: (state) => state.settings.order === 'random',
+  },
   actions: {
     setStyle(type: string) {
       this.style = type;
