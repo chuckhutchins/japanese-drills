b2fdd9fbbf1892c88dfcf886a051013526cc966f
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-01-20 12:49:35 -0500

    adding account store

---
+++ src/stores/AccountStore.ts
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia';
+import type { MaybeUser } from '@/models';
+
+type AccountState = {
+  user: MaybeUser;
+};
+
+export const useAccountStore = defineStore('account', {
+  state: (): AccountState => ({
+    user: undefined,
+  }),
+  getters: {
+    isLoggedIn: (state: AccountState) => !!state.user,
+  },
+  actions: {
+    login() {
+      // TODO: set up default user
+      this.user = {
+        birthday: new Date(),
+        createdDate: new Date(),
+        email: 'asdf@asdf.com',
+        name: 'Chuck Hutchins',
+        username: 'chuckchuckchuckchuck'
+      };
+      console.log('logging in');
+    },
+    logout() {
+      // TODO: wipe out user session
+      this.user = undefined;
+      console.log('logging out');
+    },
+  },
+});
