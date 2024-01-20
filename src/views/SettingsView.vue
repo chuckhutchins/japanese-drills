c6a7391ef6d37438dfe68764f369e72f51bb1956
Chuck Hutchins <46427222+chuckhutchins@users.noreply.github.com>
2024-01-19 21:23:53 -0500

    adding blank settings page

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="profile">
+    <h1>Settings</h1>
+    <RouterView />
+  </div>
+</template>
+
+<script setup lang="ts">
+// :)
+</script>
+
+<style scoped lang="scss">
+// :)
+</style>
